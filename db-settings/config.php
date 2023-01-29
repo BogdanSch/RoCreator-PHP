7a7761458462fe691ff072a35283860e9c594a8f
Bohdan <bogmira777@gmail.com>
2023-01-29 16:08:48 +0100

    Placed css files to scss

---
+++ db-settings/config.php
@@ -0,0 +1,5 @@
+<?php
+const SERVERNAME = "127.0.0.1";
+const DBNAME = "ro_news";
+const USERNAME = "root";
+const PASSWORD = "";
---
+++ db-settings/dbconnect.php
@@ -0,0 +1,11 @@
+<?php
+require_once "config.php";
+
+try {
+    $conn = new mysqli(SERVERNAME, USERNAME, PASSWORD, DBNAME);
+    if ($conn->connect_error) {
+        throw new Exception("Connection failed: " . $conn->connect_error);
+    }
+} catch (Exception $e) {
+    echo $e->getMessage();
+}
