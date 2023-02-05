868cb4246d761d6ae1e42a6879487f19ff495422
Bohdan <bogmira777@gmail.com>
2023-02-05 12:37:32 +0100

    Add privacy page and styles modification

--- privacy.php
+++ privacy.php
@@ -1,3 +1,19 @@
-<?php include("page-parts/header.php");?>
-
-<?php include("page-parts/footer.php");?>
+<?php include("page-parts/header.php"); ?>
+<section class="privacy">
+    <div class="container">
+        <div class="privacy_wrap">
+            <div class="content">
+                <h1>Privacy</h1>
+                <p>
+                    Read the privacy policy up here.
+                    We don't collect userdata and use it at all.
+                    We don't collect information about user location.
+                    And enjoy your experience on this web-site and our software.
+                </p>
+                <a class="btn btn--more" href="index.php">Home</a>
+            </div>
+            <img src="img/image-intro.png" class="intro-img" alt="intro-img">
+        </div>
+    </div>
+</section>
+<?php include("page-parts/footer.php"); ?>
